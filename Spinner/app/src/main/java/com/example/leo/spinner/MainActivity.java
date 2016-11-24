9af2fecfeee80621b40ea592b941e987e1dc1b7a
Leo Paulisic <leopaulisic@gmail.com>
2016-11-24 17:44:36 +0100

    Video 104.-105. Spinner

---
+++ Spinner/app/src/main/java/com/example/leo/spinner/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.leo.spinner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener{
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.days,android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView myText = (TextView) view;
+        Toast.makeText(this,"You selected "+myText.getText(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
