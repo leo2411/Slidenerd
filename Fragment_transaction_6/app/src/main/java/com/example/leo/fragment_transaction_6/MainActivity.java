3a8647730cc53fa9e2f7aa628897053beb2e4c31
Leo Paulisic <leopaulisic@gmail.com>
2016-12-08 21:29:15 +0100

    Video 126...129. FragmentManager with BackStack

--- Fragment_transaction_6/app/src/main/java/com/example/leo/fragment_transaction_6/MainActivity.java
+++ Fragment_transaction_6/app/src/main/java/com/example/leo/fragment_transaction_6/MainActivity.java
@@ -5,11 +5,14 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements FragmentManager.OnBackStackChangedListener{
 
     FragmentManager manager;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         manager = getFragmentManager();
+        manager.addOnBackStackChangedListener(this);
+        text = (TextView) findViewById(R.id.message);
     }
 
     public void addA(View v)
@@ -24,6 +29,7 @@ public class MainActivity extends Activity {
         FragmentA f1 = new FragmentA();
         FragmentTransaction transaction=manager.beginTransaction();
         transaction.add(R.id.group,f1,"A");
+        transaction.addToBackStack("addA");
         transaction.commit();
     }
     public void addB(View v)
@@ -31,6 +37,7 @@ public class MainActivity extends Activity {
         FragmentB f2 = new FragmentB();
         FragmentTransaction transaction=manager.beginTransaction();
         transaction.add(R.id.group,f2,"B");
+        transaction.addToBackStack("addB");
         transaction.commit();
     }
     public void removeA(View v)
@@ -39,6 +46,7 @@ public class MainActivity extends Activity {
         FragmentTransaction transaction=manager.beginTransaction();
         if(f1!=null){
             transaction.remove(f1);
+            transaction.addToBackStack("removeA");
             transaction.commit();
         }
         else
@@ -52,6 +60,7 @@ public class MainActivity extends Activity {
         FragmentTransaction transaction=manager.beginTransaction();
         if(f1!=null){
             transaction.remove(f1);
+            transaction.addToBackStack("removeB");
             transaction.commit();
         }
         else
@@ -64,6 +73,7 @@ public class MainActivity extends Activity {
         FragmentB f2 = new FragmentB();
         FragmentTransaction transaction=manager.beginTransaction();
         transaction.replace(R.id.group,f2,"B");
+        transaction.addToBackStack("replaceAwB");
         transaction.commit();
     }
     public void replaceBwA(View v)
@@ -71,6 +81,7 @@ public class MainActivity extends Activity {
         FragmentA f1 = new FragmentA();
         FragmentTransaction transaction=manager.beginTransaction();
         transaction.replace(R.id.group,f1,"A");
+        transaction.addToBackStack("replaceBwA");
         transaction.commit();
     }
     public void attachA(View v)
@@ -79,6 +90,7 @@ public class MainActivity extends Activity {
         FragmentTransaction transaction=manager.beginTransaction();
         if(f1!=null){
             transaction.attach(f1);
+            transaction.addToBackStack("attachA");
             transaction.commit();
         }
         else
@@ -93,11 +105,36 @@ public class MainActivity extends Activity {
         FragmentTransaction transaction=manager.beginTransaction();
         if(f1!=null){
             transaction.detach(f1);
+            transaction.addToBackStack("detachA");
             transaction.commit();
         }
         else
         {
-            Toast.makeText(this,"This fragment A is not deattached",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"This fragment A is not dettached",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void back(View v)
+    {
+        manager.popBackStack();
+    }
+
+    public  void popAddB(View v)
+    {
+        manager.popBackStack("addB",0);
+    }
+
+    @Override
+    public void onBackStackChanged() {
+
+        text.setText(text.getText()+"\n");
+        text.setText(text.getText()+ "The current status of back stack");
+        int count = manager.getBackStackEntryCount();
+        for(int i=count-1;i>=0;i--)
+        {
+           FragmentManager.BackStackEntry entry = manager.getBackStackEntryAt(i);
+            text.setText(text.getText()+ " "+ entry.getName()+" \n");
+        }
+        text.setText(text.getText()+"\n");
     }
 }
