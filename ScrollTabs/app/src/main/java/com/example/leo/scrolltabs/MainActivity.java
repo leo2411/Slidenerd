820225e22ab6c77b5067a0617b845b706510ef34
Leo Paulisic <leopaulisic@gmail.com>
2016-12-11 20:13:03 +0100

    Video 137.-139. Scrollable Tabs
    ViewPager,MyAdapter,fragmentManager,FragmentStatePagerAdapter

---
+++ ScrollTabs/app/src/main/java/com/example/leo/scrolltabs/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.leo.scrolltabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
+
+public class MainActivity extends FragmentActivity {
+
+    ViewPager viewPager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        viewPager.setAdapter(new MyAdapter(fragmentManager));
+    }
+}
+
+
+
+class MyAdapter extends FragmentStatePagerAdapter
+{
+
+    public MyAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        Fragment fragment = null ;
+        if(position==0){
+
+            fragment = new FragmentA();
+        }
+        if(position==1){
+
+            fragment = new FragmentB();
+        }
+        if(position==2){
+
+            fragment = new FragmentC();
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position==0){
+
+            return "Tab 1";
+
+        }
+        if(position==1){
+
+            return "Tab 2";
+        }
+        if(position==2){
+
+            return "Tab 3";
+        }
+        return null;
+    }
+}
